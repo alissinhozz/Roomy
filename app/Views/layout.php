840bcce2411f4d67d0ac7ffc9c734821f9346045
Beatriz Miranda <mirandabeatrizaparecida@gmail.com>
2022-12-03 17:33:01 -0300

    aaaa

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -17,7 +17,7 @@
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container px-4 px-lg-5">
-            <img src="assets\loguinho.png" alt="logo" width=180 height=90>
+            <img src="\assets\loguinho.png" alt="logo" width=180 height=90>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
                     <li class="nav-item"><a class="nav-link active" aria-current="page" href="<?php echo base_url()?>">Home</a></li>
