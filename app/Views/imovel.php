93cca7fbcc5c54d62380981558db50a4618eb890
gabilse <gabrieli.b@aluno.ifsc.edu.br>
2022-12-03 20:52:48 -0300

    É para conter as imgs

--- app/Views/imovel.php
+++ app/Views/imovel.php
@@ -11,6 +11,7 @@
         <link rel="icon" type="image/x-icon" href="assets\loguinho.png" />
         <!-- Bootstrap icons-->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="\css\styles.css" rel="stylesheet" />
     </head>
